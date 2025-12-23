7fd3ef0eefcb559bb72973b4a827d3167f78c0de
VIKRAM2605 <vikrams.al24@gmail.com>
2025-12-23 15:38:52 +0530

    made it to work with ngrok

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'unpayable-nonradically-felisha.ngrok-free.dev',
+    ],
+  }
 })
