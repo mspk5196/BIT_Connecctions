0acd72000394b28a2bad7202629c64f386a32277
Mithul-varshan <mithul0605@gmail.com>
2026-01-10 13:27:39 +0530

    fixed /assign to contact/assign

--- client/src/components/Forms/DetailsInput.jsx
+++ client/src/components/Forms/DetailsInput.jsx
@@ -1087,7 +1087,7 @@ function DetailsInput() {
   const handleAssignToUser = async () => {
     if (source === "middleman" && initialData && currentUserId) {
       try {
-        const response = await api.post(`/assign/`, {
+        const response = await api.post(`contact/assign/`, {
           assigned_by: currentUserId,
           assigned_to: initialData.events[0]?.created_by,
           event_id: initialData.events[0]?.event_id,
