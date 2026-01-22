fad1832fe8cde820a1714fef07426e8e164d0c75
Mithul-varshan <mithul0605@gmail.com>
2026-01-22 12:13:26 +0530

    change corse cofig to allow only pcdp

--- server/server.js
+++ server/server.js
@@ -30,14 +30,14 @@ dotenv.config();
 //     allowedHeaders: ["Content-Type", "Authorization"],
 //   })
 // );
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 // const allowedOrigins = [
 //   "http://localhost:5173",
 //   "http://localhost:5174",
@@ -45,6 +45,14 @@ app.use(
 //   "http://10.208.71.214:5173",
 //   "https://unpayable-nonradically-felisha.ngrok-free.dev",
 // ];
+app.use(
+  cors({
+    origin:"https://pcdp.bitsathy.ac.in",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // app.use(
 //   cors({
